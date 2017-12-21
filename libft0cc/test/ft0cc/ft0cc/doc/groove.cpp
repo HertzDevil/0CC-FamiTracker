6912fdd18a4d65b817c7558ddb52967b7ed5d19b
HertzDevil <nicetas.c@gmail.com>
2017-12-21 21:39:58 +0800

    continuous tests

---
+++ libft0cc/test/ft0cc/ft0cc/doc/groove.cpp
@@ -0,0 +1,20 @@
+#include "ft0cc/doc/groove.hpp"
+#include "gtest/gtest.h"
+
+using groove = ft0cc::doc::groove;
+
+TEST(Groove, DefaultCtor) {
+	auto grv = groove { };
+	EXPECT_EQ(grv.size(), 0u);
+	EXPECT_DOUBLE_EQ(grv.average(), groove::default_speed);
+}
+
+TEST(Groove, DefaultCtorWithSpeed) {
+	auto grv = groove {2};
+	EXPECT_EQ(grv.size(), 1u);
+	EXPECT_DOUBLE_EQ(grv.average(), 2.);
+}
+
+TEST(Groove, Fail) {
+	FAIL();
+}
