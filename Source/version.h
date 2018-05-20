024065cf71e35ab3d173c82e509c83c97e472ebe
HertzDevil <nicetas.c@gmail.com>
2018-05-20 15:41:07 +0800

    0.4.0.0

--- Source/version.h
+++ Source/version.h
@@ -26,13 +26,13 @@
 // Application version information
 
 // Define this for beta builds
-// #define WIP
+#define WIP
 
 // Version info
 #define VERSION_API  0
-#define VERSION_MAJ  3
-#define VERSION_MIN  15
-#define VERSION_REV  2
+#define VERSION_MAJ  4
+#define VERSION_MIN  0
+#define VERSION_REV  0
 
 constexpr int Compare0CCFTVersion(int api, int maj, int min, int rev) noexcept {
 	if (api > VERSION_API)
